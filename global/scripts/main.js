4feceeef0b3943ae0890fb2fadde27731da7103f
Troy Yauk <troyyauk@gmail.com>
2026-05-16 15:35:45 -0400

    Update main.js

--- global/scripts/main.js
+++ global/scripts/main.js
@@ -1,81 +1,22 @@
 import jsonConfig from 'https://t-yauk.github.io/peak-theatre/library.json' with {type: "json"};
-const filter = localStorage.getItem('filter');
+const library = jsonConfig.movies;
 const container = (document.getElementsByClassName("library"))[0];
-const pw = (document.getElementsByClassName("profile-wrapper"))[0];
-let library;
-let movieItems;
-let id;
 
 window.onload = function() {
-
-	if(filter == "all" || filter == null){
-		localStorage.setItem('filter', 'all');
-		library = jsonConfig.movies;
-	}
-
-	console.log(filter);
 
 	populate();
 
 }
 
-function populate(){
+function populate() {
 
 	for(let i=0;i<library.length;i++){
+
 		const newItem = document.createElement('div');
-		newItem.classList.add("movie-item");
-		newItem.id = i;
-		newItem.style.backgroundImage = "url('" + library[i].previewImage + "')";
-		newItem.innerHTML = "<div class='gradient'></div><span class='title'>" + library[i].title + "</span>";
+		newItem.classList.add("apple-tv-card-container");
+		newItem.innerHTML = "<div class='apple-tv-card'><a class='content' style='background-image:url('" + library[i].previewImage + "');'></a><div class='parallax-content'><div class='content-wrapper'><span class='title'>" + library[i].title + "</span></div></div>";
 		container.appendChild(newItem);
+
 	}
 
-	movieItems = document.querySelectorAll('.movie-item');
-	movieItems.forEach(element => {
-		element.addEventListener('click', function(event) {
-			id = Number(this.id);
-			populateProfile();
-			console.log(id);
-  		});
-	});
-
 }
-
-
-function populateProfile(){
-
-	document.getElementById("image").src = library[id].image_url;
-	document.getElementById("title").innerHTML = library[id].title;
-	document.getElementById("year").innerHTML = library[id].year;
-	document.getElementById("description").innerHTML = library[id].description;
-	setTimeout(function() {
-		showprofile();
-	}, 100);
-
-}
-
-function showprofile(){
-
-	pw.classList.add("active");
-
-}
-
-const parent = document.querySelector('.profile-wrapper');
-
-parent.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget) {
-    pw.classList.remove("active");
-  }
-});
-
-
-window.watch = function () {
-	const url = "watch.html?" + id;
-	window.location.href = url;
-}
-
-
-
-
-
-
